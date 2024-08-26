281166acf5b68c59f85b0c2e2f53659f6e3f230f
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2024-08-26 01:55:25 +0100

    Update about and README

--- src/pages/aboutPage/AboutPage.tsx
+++ src/pages/aboutPage/AboutPage.tsx
@@ -8,11 +8,11 @@ const AboutPage: React.FC = () => {
         <h1 className="about-heading">Encrypted Data Sharing</h1>
         <h2 className="about-subheading">Secure and Private Data Exchange</h2>
         <p className="about-description">
-          Encrypted Data Sharing (EDS) is a platform designed to provide secure
-          and private data exchange which ensures that your data remains
-          protected through end-to-end encryption. Whether you're sharing files
-          with colleagues or storing sensitive information, EDS ensures that
-          your data is safe from unauthorized access.
+          EDS is a platform designed to provide secure and private data exchange
+          which ensures that your data remains protected through end-to-end
+          encryption. Whether you're sharing files with colleagues or storing
+          sensitive information, EDS ensures that your data is safe from
+          unauthorized access.
         </p>
       </div>
     </div>
